31bcc60d4aef863c566dc3b075baed2e342dd4f2
Neil <neilkosh001@gmail.com>
2018-09-11 16:06:03 -0400

    updated htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,22 +1,17 @@
-//Dependencies
 var path = require("path");
 
 
-//Routing
 
 module.exports = function(app) {
 
-  //Root to index
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  //Survey
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
-  // If no matching route is found default to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
